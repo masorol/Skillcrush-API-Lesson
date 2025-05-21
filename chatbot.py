88e39160609309c25fe34d7a897dccd2fb4d6ce1
Mia Sorola Yoshida <149900982+masorol@users.noreply.github.com>
2025-05-21 08:56:14 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,13 @@ from openai import OpenAI
 
 client = OpenAI()
 
+def set_user_input_category(user_input):
+    question_keywords = ["who", "what", "when", "where", "why", "how", "?"]
+    for keyword if question_keywords:
+        if keyword in user_input.lower():
+            return "question"
+        return "statement"
+
 def get_response(model, messages):
     response = client.chat.completions.create(
         model = model,
@@ -14,11 +21,14 @@ user_input = input("\nAsk something...\n\n")
 model = "gpt-3.5-turbo"
 
 messages = [
-    {"role": "system", "content": "You are an assistant that always answers in the form of a poem."},
+    {"role": "system", "content": "You are an assistant that talks like a Shakespearian pirate."},
     {"role": "user", "content": user_input}
 ]
 
 response_for_user = get_response(model, messages)
+
+if set_user_input_category(user_input) == "question":
+    response_for_user = "Good question! " + response_for_user
 
 print("\n" + response_for_user + "\n")
 
