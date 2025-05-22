2af0699b9e3affed54e59059f18ecbe43c83f963
masorol <miasorola.yoshida@gmail.com>
2025-05-22 15:36:34 -0500

    commit assistant.py

---
+++ Lesson8/assistant.py
@@ -0,0 +1,98 @@
+import time
+import random
+from openai import OpenAI
+import logging
+import datetime
+import re
+
+log = logging.getLogger("assistant")
+
+logging.basicConfig(filename = "assistant.log", level = logging.INFO)
+
+client = OpenAI()
+
+def process_run(thread_id, assistant_id):
+    new_run = client.beta.threads.runs.create(
+        thread_id = thread_id,
+        assistant_id = assistant_id
+    )
+
+    phrases = ["Thinking", "Pondering", "Dotting the i's", "Achieving world peace"]
+
+    while True:
+        time.sleep(1)
+        print(random.choice(phrases) + "...")
+        run_check = client.beta.threads.runs.retrieve(
+            thread_id = thread_id,
+            run_id = new_run.id
+        )
+        if run_check.status in ["cancelled", "failed", "completed", "expired"]:
+            return run_check
+
+def log_run(run_status):
+    if run_status in ["cancelled", "failed", "expired"]:
+        log.error(str(datetime.datetime.now()) + " Run " + run_status + "\n")
+
+def get_message(run_status):
+    if run_status == "completed":
+        thread_messages = client.beta.threads.messages.list(
+            thread_id = thread.id
+        )
+        message = thread_messages.data[0].content[0].text.value
+
+        if thread_messages.data[0].content[0].text.annotations:
+            pattern = r'【[\d:]+†source】'
+            message = re.sub(pattern, '', message)
+
+
+    if run_status in ["cancelled", "failed", "expired"]:
+        message = "An error has occurred, please try again."
+    
+    return message
+
+assistant = client.beta.assistants.retrieve(assistant_id = "asst_FANWSczvwTSKpuAeTDCm0TdM")
+
+# file = client.vector_stores.files.create_and_poll(
+#  vector_store_id="vs_682f7dde49448191a3e5751cbb6600af",
+#  file_id="file-LKhqXGDSNhKfF439uytUyz"
+# )
+
+# vector_store_files = client.vector_stores.files.list(
+#     vector_store_id="vs_682f7dde49448191a3e5751cbb6600af"
+# )
+
+# print(vector_store_files)
+# exit()
+
+thread = client.beta.threads.create()
+
+user_input = ""
+
+2
+3
+while True:
+    if (user_input == ""):
+        print("Assistant: Hello there! Just so you know, you can type exit to end our chat. What's your name? ")
+        name = input("You: ")
+        print("Assistant: Hey, " + name + "! How can I help you?")
+        user_input = input("You: ")
+    else:
+        user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        print("Goodbye!")
+        exit()
+
+    message = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role = "user",
+        content = user_input
+    )
+
+    run = process_run(thread.id, assistant.id)
+
+    log_run(run.status)
+
+    message = get_message(run.status)
+
+    print("\nAssistant: " + message + "\n")
